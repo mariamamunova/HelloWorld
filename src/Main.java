c31372bd687a8058636d4f8681454fb5997a7319
MariaMamunova <mariam493@nycstudents.net>
2025-09-09 15:32:10 -0400

    added reading from a text file

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,6 @@ public class Main
         File f = new File("Class.txt");
         s = new Scanner(f);
         String course = s.nextLine();
-        System.out.println("Welcome to" + course + "!");
+        System.out.println("Welcome to " + course + "!");
     }
 }
